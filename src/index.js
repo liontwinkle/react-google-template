ef9bfbf95375f4a505443d30ad584760309411a4
liontwinkle <lion.twinkle.ge@gmail.com>
2020-04-30 12:43:58 +0300

    React preloader integrated

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import CommandPost from './components/CommandPost';
+import {Dots} from 'react-preloaders';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <CommandPost />
+    <Dots color={'#0168fa'} background="#ffffff" />
   </React.StrictMode>,
   document.getElementById('root')
 );
