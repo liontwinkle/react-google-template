02425f7a34a6c9ddf2cb391bc89d4d2834da67e0
liontwinkle <lion.twinkle.ge@gmail.com>
2020-05-11 06:18:53 +1000

    Dashboard info added

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import WidePane from '../../layout/WidePane';
 
 class Dashboard extends Component {
   render() {
     return (
       <WidePane animateClass={this.props.animateClass}>
-        Dashboard
+        <div className="alert alert-secondary m-3 wd-300" role="alert">
+          <strong>Event:</strong> _ (_)
+		      <br /><strong>Instance:</strong> _ - _ (_)
+		      <br /><strong>Team:</strong> _ (_)
+		      <br /><strong>User:</strong> {this.props.userData.user_first_name} {this.props.userData.user_last_name} ({this.props.userData.user_role})
+	      </div>
       </WidePane>
     );
   }
 };
 
-export default Dashboard;
+const mapStateToProps = state => {
+  return {
+    userData: state.auth.userData
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
