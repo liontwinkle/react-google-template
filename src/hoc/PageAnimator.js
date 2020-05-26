6e6be93cf9fe677d9007287899cd0576a4afd673
liontwinkle <lion.twinkle.ge@gmail.com>
2020-05-26 17:00:12 +0400

    clean up repo

--- src/hoc/PageAnimator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PageAnimator = (props) => (
-    <div className={props.animateClass}>
-            {props.children}
-    </div>
-);
-
-export default PageAnimator;
