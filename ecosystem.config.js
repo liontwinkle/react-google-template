8a0bba5ce7c248ecbd57de78605c6aa21d99b2f3
liontwinkle <lion.twinkle.ge@gmail.com>
2020-06-01 18:38:30 +0400

    deploying production settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,20 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    name: 'commandpost_app',
+    script: 'npm',
+    args: 'run start:production',
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }],
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
+      user : 'root',
+      host : '74.208.102.130',
       ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+      repo : 'https://github.com/i-need-helpers/commandpost_app.git',
+      path : '/var/www/vhosts/commandpost.com.au/cp_client',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
