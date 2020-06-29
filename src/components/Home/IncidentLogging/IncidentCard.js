a62d85ccacd0838a2606c18b71b235e411d40d95
liontwinkle <lion.twinkle.ge@gmail.com>
2020-06-29 03:17:32 +0200

    fixing some style more

--- src/components/Home/IncidentLogging/IncidentCard.js
+++ src/components/Home/IncidentLogging/IncidentCard.js
@@ -6,11 +6,28 @@ const IncidentCard = ({children, title}) => {
     return (
         <div className="incident-card">
             <div className="incident-card-header">
-                <FontAwesomeIcon icon={faHome} />
-                <span>{ title }</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                     stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                     className="feather feather-home" fill="none">
+                    <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    <polyline points="9 22 9 12 15 12 15 22" />
+                </svg>
+                <span className="font-weight-bold">{ title }</span>
                 <span className="incident-card-header-action">
-                    <FontAwesomeIcon icon={faReply} />
-                    <FontAwesomeIcon icon={faBars} />
+                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                        className="feather feather-corner-up-left"><polyline points="9 14 4 9 9 4" /><path
+                       d="M20 20v-7a4 4 0 0 0-4-4H4" /></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                         className="feather feather-list"><line x1="8" y1="6" x2="21" y2="6" /><line x1="8" y1="12"
+                                                                                                          x2="21"
+                                                                                                          y2="12" /><line
+                        x1="8" y1="18" x2="21" y2="18" /><line x1="3" y1="6" x2="3" y2="6" /><line x1="3"
+                                                                                                             y1="12"
+                                                                                                             x2="3"
+                                                                                                             y2="12" /><line
+                        x1="3" y1="18" x2="3" y2="18" /></svg>
                 </span>
             </div>
             <div className="incident-card-body">
