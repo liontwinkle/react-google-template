fd97c225d58e9600ab873d879c648455a82c6e16
liontwinkle <lion.twinkle.ge@gmail.com>
2020-07-15 11:45:25 +0200

    update the style issues

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -26,9 +26,6 @@ import NotFound from '../NotFound';
 import * as routes from '../../constants/routes';
 import * as loginSteps from '../../constants/login_steps';
 
-import './App.scss';
-import './Layout.scss';
-
 function App({
   loading,
   teamFg,
