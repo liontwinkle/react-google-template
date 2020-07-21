10c175ea42068f7a9180b616d65f50564440c4ba
liontwinkle <lion.twinkle.ge@gmail.com>
2020-07-21 17:41:31 +0300

    Loader gif and Lottie

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import * as animationData from './animationData.json';
 import './Loader.scss';
 
 function Loader() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData.default,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
   return (
-   <div className="loader" />
+    <div className="loader">
+      <Lottie options={defaultOptions} />
+    </div>
   );
 }
 
